f0904e59a74c108f57b1e07502982deb100601c3
Spheroidon <spheroidon@gmail.com>
2026-09-03 19:46:35 +0300

    Added more changelogs from the modpack's internal docs

--- changelogs.js
+++ changelogs.js
@@ -1,6 +1,6 @@
 const changelog_1 = {
     "title": "Version 1 - Init",
-    "date": new Date(2026, 4, 8, 15, 30),
+    "date": new Date(2026, 7, 5, 22, 15),
     "content": `Added (Server):
     - Just Enough Items
     - JourneyMap
@@ -48,7 +48,7 @@ const changelog_1 = {
 
 const changelog_2 = {
     "title": "Version 2 - QOL",
-    "date": new Date(2026, 4, 8, 17, 20),
+    "date": new Date(2026, 7, 5, 22, 45),
     "content": `Added (Server):
     - Better Combat
     - Combat Roll
@@ -140,4 +140,252 @@ const changelog_2 = {
     - Turned off "Themed Main Menu" in Aquamirae`
 }
 
-export const changelogs = [changelog_1, changelog_2]
+const changelog_3 = {
+    "title": "Version 3 - Alexander's Mods",
+    "date": new Date(2026, 7, 5, 23, 10),
+    "content": `Added (Server):
+    - Neruina
+    - Curios API
+    - Citadel
+    - Alex's Mobs
+    - Alex's Caves
+    - Ice and Fire: Dragons
+    - Domestication Innovation
+    - Rats
+    - Cloud Storage
+    - I Wanna Skate
+    - Local Looks
+    - Wiki Zoomer
+    - TACT
+    - Patchouli
+    - Akashic Tome
+    - Morph-o-Tool
+    - Psi
+    - Psi: Tweaks and Additions
+    - Botania
+    - Botania: Garden of Glass
+    - Mowzie's Mobs
+    - Lionfish API
+    - L_Ender's Cataclysm
+    - Cataclysm & BetterCombat - Compatibility
+    - Alex's Caves Better Combat
+    - Mowzie's Cataclysm
+    - Born in Chaos
+    - Royal Variations
+    - Gnumus Settlement
+    - Clanging Howl
+    - Warriors of Past Epoch
+    - Eternal Hunts
+    - Born in Configuration
+    - Alex's Mobs Interactions
+    - Alex's Caves Exemplified
+    
+    Added (Client):
+    - Retro Damage Indicators
+    
+    Config Changes:
+    - Turned on "Ratlantis Datapack Enabled by Default" in Rats
+    - Enabled "Restore End Story" in TACT
+    - Disabled "Custom main menu" in Ice and Fire: Dragons
+    - Changed "Generate Dragon Cave Chance" from 180 to 540
+    - Changed "Generate Dragon Roost Chance" from 360 to 1080
+    - Changed "Generate Dragon Skeleton Chance" from 300 to 2000
+    - Changed "Dangerous World Gen Dist From Spawn" from 800 to 5000
+    - Changed "Dangerous World Gen Dist Seperation" from 300 to 800
+    - Turned off "EFFECTS REMINDER ENABLED" in Alex's Mobs Interaction
+    - Turned off "PATCHOULI REMINDER ENABLED" in Alex's Caves Exemplified
+    - Turned off "Single Use Mirror" in Local Looks
+    - Turned off "Give Magic Mirror on Game Start" in Local Looks`
+}
+
+const changelog_4 = {
+    "title": "Version 4 - Abnormally Scorched",
+    "date": new Date(2026, 7, 6, 11, 50),
+    "content": `Added (Server):
+    - Feature Recycler
+    - Item Split Bug Fix
+    - Just Enough Resources
+    - Architectury API
+    - Resourceful Lib
+    - Blueprint
+    - YUNG's API
+    - Framework
+    - Overweight Farming
+    - Oreganized
+    - Carcinogenius
+    - Brazier
+    - Etcetera
+    - Thigh highs etc.
+    - Doom & Gloom
+    - Gallery
+    - Caverns & Chasms
+    - Boatload
+    - Upgrade Aquatic
+    - Incubation
+    - Savage & Ravage
+    - Atmospheric
+    - The Endergetic Expansion
+    - Woodworks
+    - Allurement
+    - Pet Cemetery
+    - Clayworks
+    - Buzzier Bees
+    - Autumnity
+    - Personality
+    - Neapolitan
+    - Environmental
+    - Berry Good
+    - Scorched Guns 2
+    - Scorched Guns: Oregunized
+    - Scorched Guns: Caverns & Chasms Compat
+    - Scorched Guns: Blistering Armor
+    - Structurify
+    - Villages&Pillages
+    - Variants&Ventures
+    - Friends&Foes
+    - Friends&Foes - Beekeeper Hut
+    - Friends&Foes - Flowery Mooblooms
+    - Legendary Monsters
+    - Legendary Monsters Addons
+    - Hot Kettles
+    - Clay Soldiers Reborn
+    - Bird Nests
+    - Cryonic Config
+    - Skarrier Mobs
+    - Metus Oblita
+
+    Added (Client):
+    - CreativeTabs+
+
+    Config Changes:
+    - Turned off "Labels Enabled" in Obscure Tooltips`
+}
+
+const changelog_5 = {
+    "title": "Version 5 - YUNG's Delight",
+    "date": new Date(2026, 7, 6, 13, 40),
+    "content": `Added (Server):
+    - JEAsync
+    - Global Packs
+    - Abnormal Fruits
+    - Eggcelent (Compat)
+    - Nest Egg
+    - WITS
+    - RunicLib
+    - Mysterious Mountain Lib
+    - Recipe Modification Library
+    - Delight Lib
+    - CarrasconLib
+    - Structure Credits
+    - Orpheus
+    - Farmer's Delight
+    - Delightful Fixes
+    - Farmer's Respite
+    - Abnormals Delight
+    - Carrot Rarity
+    - Crafty Hands
+    - Thicket
+    - Frycook's Delight
+    - Brewin' and Chewin'
+    - Chewy Cheeses
+    - Cocktails Delight
+    - Smidgeon o' Bliss REMASTERED
+    - Collector's Reap
+    - Chef's Delight
+    - Veggies Delight
+    - Fungi Delight
+    - Ramadan Delight
+    - Unusual Delight
+    - Spanish Delight
+    - Pizza Delight
+    - Nether's Delight
+    - Ocean's Delight
+    - Delightful
+    - Alex's Delight
+    - Ender's Delight
+    - End's Delight
+    - Cultural Delights
+    - Miner's Delight +
+    - Autochef's Delight
+    - Corn Delight
+    - Quark Delight
+    - Cuisine Delight
+    - Pineapple Delight
+    - Rustic Delight
+    - Fruits Delight
+    - More Delight
+    - Vintage Delight
+    - Ube's Delight
+    - Dungeons Delight
+    - Casualness Delight
+    - L_Ender's Cataclysm Delight
+    - New Year's Delight
+    - Rice Delight
+    - Roll Delight
+    - Crabber's Delight
+    - Barbeque's Delight
+    - Cave Delight
+    - Alex's Mobs Delight
+    - Ice and Fire Delight
+    - Oceanic Delight
+    - Legume Delight
+    - Vegan Delight
+    - Sunflower Delight
+    - Seed Delight
+    - Dumplings Delight Reload
+    - Berries & Cherries Delight
+    - Hotdog Delight
+    - Slime Delight
+    - Vanilla Delight
+    - Caves Delight
+    - Chopper's Delight
+    - Easter's Delight
+    - Peruvian's Delight
+    - Large Meals
+    - TerraBlender
+    - YUNG's Better Mineshafts
+    - YUNG's Better Dungeons
+    - YUNG's Better Nether Fortresses
+    - YUNG's Better Ocean Monuments
+    - YUNG's Better End Island
+    - YUNG's Better Strongholds
+    - YUNG's Better Jungle Temples
+    - YUNG's Better Desert Temples
+    - YUNG's Better Witch Huts
+    - YUNG's Better Caves
+    - YUNG's Bridges
+    - YUNG's Extras
+    - YUNG's Cave Biomes
+    - Elysium API
+    - Jaden's Nether Expansion
+    - Jaden's Nether Expansion Delight
+    - Dungeons Now Loading
+    - Delights Now Loading
+    
+    Added (Client):
+    - Particle Rain
+    - Fog Looks Fixed Now
+    - Traveler's Titles
+    - YUNG's Menu Tweaks
+    - (Resourcepack, Forced, Custom) UpgradeAquaticAxolotlFix
+    
+    Updated:
+    - JourneyMap (6.0.0-beta.6 to 6.0.0)
+    
+    Removed:
+    - Just Enough Items
+    
+    Config Changes:
+    - Enabled "Resummoned Dragon Drops Egg" in YUNG's Better End Island
+    - Turned off "Disable Vanilla Nether Fortresses" in YUNG's Better Nether Fortresses
+    - Turned off "Disable Vanilla Witch Huts" in YUNG's Better Witch Huts
+    - Turned off "Disable Vanilla Mineshafts" in YUNG's Better Mineshafts
+    - Turned off "Disable Vanilla Ocean Monuments" in YUNG's Better Ocean Monuments
+    - Turned off "Disable Vanilla Jungle Temples" in YUNG's Better Jungle Temples
+    - Turned off "Disable Vanilla Pyramids" in YUNG's Better Desert Temples
+    - Turned on "Enable Small Nether Dungeons" in YUNG's Better Dungeons`
+}
+
+
+
+export const changelogs = [changelog_1, changelog_2, changelog_3, changelog_4, changelog_5]
